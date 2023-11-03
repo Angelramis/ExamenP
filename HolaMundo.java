63567f7f7e7a53a64ea9bc15177d5c94eab875f8
Angel <asegui20218@iesjoanramis.org>
2023-11-03 11:27:02 +0100

    Afegit el nom de l'autor

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		 System.out.println("Canvi 1");
+		 System.out.println("Angel");
 		//Comentari una linea
 		
 		/*Comentari diverses linies
