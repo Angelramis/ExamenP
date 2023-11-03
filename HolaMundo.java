357e64e0db666d6af7f4a8b279f5e9807b379c82
Angel <asegui20218@iesjoanramis.org>
2023-11-03 12:56:42 +0100

    programa producte

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,14 +2,18 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
-		 System.out.println("Angel");
-		//Comentari una linea
-		
-		/*Comentari diverses linies
-		 * Un altre comentari
-		 * Hola!!!!!
-		 */
+	System.out.print("Introdueix el primer número: ");
+        double numero1 = entrada.nextDouble();
+
+        System.out.print("Introdueix el segon número: ");
+        double numero2 = entrada.nextDouble();
+
+        double producte = numero1 * numero2;
+
+        System.out.println("El producte és: " + producte);
+
+        entrada.close();
+
 	}
 
 }
