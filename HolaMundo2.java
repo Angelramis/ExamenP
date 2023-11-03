98fe4e47421498bb7d45adaf531f7174f35ad6cc
Angel <asegui20218@iesjoanramis.org>
2023-11-03 11:47:28 +0100

    Ex 5 nou canvi

---
+++ HolaMundo2.java
@@ -0,0 +1,15 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+		 System.out.println("Angel");
+		//Comentari una linea
+		
+		/*Comentari diverses linies
+		 * 
+		 * Hola
+		 */
+	}
+
+}
